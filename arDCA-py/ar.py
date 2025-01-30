6b60406aa99c89f775fffc6b3a2506d0802cb019
tferracina <tferracina@icloud.com>
2025-01-30 12:36:58 +0100

    feat: defined ardca functions

---
+++ arDCA-py/ar.py
@@ -0,0 +1,52 @@
+import numpy as np
+from typing import Union, Tuple
+
+from types import ArVar, ArAlg, ArNet
+
+
+def ardca_zw(Z: np.ndarray, W: np.ndarray,
+            lambdaJ: float = 0.01,
+            lambdaH: float = 0.01,
+            pc_factor: float = 0,
+            epsconv: float = 1.0e-5,
+            maxit: int = 1000,
+            verbose: bool = True,
+            method: str,
+            permorder: Union[str, np.ndarray]):
+
+    #checkpermorder(permorder)
+    
+    # Check W is normalized
+    if (W < 0).any():
+        raise ValueError("weights should be positive")    
+    if not np.sum(W) == 1:
+        raise ValueError("weights should be normalized")
+
+    Z_copy = np.copy(Z)
+    N, M = Z_copy.shape
+    if M != len(W):
+        raise ValueError("columns in Z should match length of W")
+
+    q = int(Z_copy.max())
+    
+    #Initialize algorithm parameters in ArAlg object
+    aralg = ArAlg(method, verbose, epsconv, maxit)
+
+    #Initialize model variables in ArVar object
+    arvar = ArVar(N, M, q, lambdaJ, lambdaH, Z_copy, W, pc_factor, permorder)
+
+    #Perform optimization, calling minimize_arnet
+    theta, psval = minimize_arnet(aralg, arvar)
+
+    #Return the result as a tuple of trained model and its parameters
+    return ArNet(theta,arvar), arvar
+
+
+def ardca_fasta(filename: str, **kwargs):
+    W, Z, _, _, _ = read_fasta(filename, kwargs.get("max_gap_fraction", 0.9),
+                               kwargs.get("theta", "auto"),
+                               kwargs.get("remove_dups", True))
+    W /= W.sum() # Normalize weights
+    return ardca_zw(Z, W, **kwargs)
+
+def minimize_arnet(alg: ArAlg, var: ArVar) -> Tuple[theta, vecps]:
